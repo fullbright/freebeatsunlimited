17228441463bd3a42abde86e8da90e994b79e442
fullbright <afanousergio@gmail.com>
2012-09-23 18:36:04 +0200

    Add survey bundle source code

---
+++ src/freebeats/SurveyBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace freebeats\SurveyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('freebeatsSurveyBundle:Default:index.html.twig');
+    }
+}
---
+++ src/freebeats/SurveyBundle/freebeatsSurveyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace freebeats\SurveyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class freebeatsSurveyBundle extends Bundle
+{
+}
