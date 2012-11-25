999a2387a3024cc9389c40041dd9519e8f37bbd2
Sergio AFANOU <afanousergio@gmail.com>
2012-11-25 18:30:56 +0100

    Improve showroom page

--- src/freebeats/ShowroomBundle/Controller/DefaultController.php
+++ src/freebeats/ShowroomBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('freebeatsShowroomBundle:Default:index.html.twig');
+        return $this->render('freebeatsShowroomBundle:Default:moulinrouge.html.twig');
     }
 }
